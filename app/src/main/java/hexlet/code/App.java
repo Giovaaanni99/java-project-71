dbcbff20b047d02e67575da56bf976f5e0af236d
Giovaaanni99 <ivan.g92@mail.ru>
2025-02-23 18:08:31 +0300

    Step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,15 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
 }
